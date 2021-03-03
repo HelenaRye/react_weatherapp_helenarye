2e0a3d9341baf6db49add6c3777b557b1f07d2d4
HelenaRye <78907672+HelenaRye@users.noreply.github.com>
2021-03-03 20:14:14 +0100

    added axios API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import React from "react";
 import Units from "./Units.js";
 import Form from "./Form.js";
 import Resultcontainer from "./Resultcontainer.js";
@@ -11,7 +12,7 @@ export default function App() {
       <div className="Container">
         <Form />
         <Units />
-        <Resultcontainer />
+        <Resultcontainer defaultCity="Copenhagen"/>
         <Forecast />
       </div>
     </div>
--- src/Resultcontainer.js
+++ src/Resultcontainer.js
@@ -1,37 +1,57 @@
+import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
 
-export default function Resultcontainer() {
-  let weatherData = {
-    city: "Copenhagen",
-    date: "Wednesday 10:00",
-    description: "Cloudy",
-    temperature: "10",
-    humidity: " 71",
-    wind: "14"
-  };
+export default function Resultcontainer(props) {
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState({ready: false});
 
-  return (
-    <div className="result-container">
-      <div className="row">
-        <div className="col-6">
-          <h1><strong>{weatherData.city}</strong></h1>
-          <p className="date">{weatherData.date}</p>
-          <p id="description">{weatherData.description}</p>
-          <h2>
-            <i className="fas fa-cloud"></i>
-            <span className="temperature-number">
-              {" "}
-              {weatherData.temperature}°
-            </span>
-          </h2>
-        </div>
-        <div className="col-md-6">
-          <ul className="wind-humidity">
-            <li>Humidity: {weatherData.humidity} % </li>
-            <li>Wind: {weatherData.wind} kmph</li>
-          </ul>
+  function handleResponse(response) {
+      setWeatherData({
+        ready: true,
+        temperature: response.data.main.temp,
+        humidity: response.data.main.humidity,
+        date: new Date(response.data.dt * 1000),
+        description: response.data.weather[0].description,
+        wind: response.data.wind.speed,
+        city: response.data.name,
+      });
+  }
+
+
+
+  if (weatherData.ready) {
+
+    return (
+      <div className="result-container">
+        <div className="row">
+          <div className="col-6">
+            <h1><strong>{weatherData.city}</strong></h1>
+            <p className="date">{weatherData.date}</p>
+            <p className="text-capitalize">{weatherData.description}</p>
+            <h2>
+              <i className="fas fa-cloud"></i>
+              <span className="temperature-number">
+                {" "}
+                {Math.round(weatherData.temperature)}°
+              </span>
+            </h2>
+          </div>
+          <div className="col-md-6">
+            <ul className="wind-humidity">
+              <li>Humidity: {weatherData.humidity} % </li>
+              <li>Wind: {weatherData.wind} kmph</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    
+    const apiKey = "42ec6cb05f191a16b64dfede975cfc2c";
+    let apiUrl = `api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading..."
+  }
 }
