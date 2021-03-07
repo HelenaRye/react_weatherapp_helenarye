7e405646819f69f0b86b0c2097f9849498d0d5f6
HelenaRye <78907672+HelenaRye@users.noreply.github.com>
2021-03-07 17:25:21 +0100

    footer css position edits

--- src/Footer.js
+++ src/Footer.js
@@ -2,16 +2,18 @@ import "./App.css";
 
 export default function Footer() {
   return (
-    <small> <h6>
-      <i class="fab fa-github"></i> 
+    <div className="footer">
+      <small>
+      <i class="fab fa-github"></i>  {" "}
       <a
-        href="https://github.com/HelenaRye/helenas_weatherapp"
+        href="https://github.com/HelenaRye/react_weatherapp_helenarye"
         className = "links" 
         target="_blank"
       >
         Open-source code
       </a>
        {" "} by Helena Rye
-    </h6></small>
+    </small>
+    </div>
   );
 }
