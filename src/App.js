0493a300b87a079be91e304e99041b569c350f5d
HelenaRye <78907672+HelenaRye@users.noreply.github.com>
2021-03-07 10:50:22 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import React from "react";
 import Units from "./Units.js";
 import Resultcontainer from "./Resultcontainer.js";
 import Forecast from "./Forecast.js";
-import WeatherInfo from './WeatherInfo';
 
 export default function App() {
   return (
